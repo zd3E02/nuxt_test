0bfce6db55040af9a8c2b940f0ddf75a996ffd6b
zd3e02 <zd3e02@gmail.com>
2018-09-12 19:22:10 +0900

    branch test to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,22 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        testtttaaaa
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <!-- <li>{{user.title.rendered}}</h1> -->
+    <h1 v-html="user.content.rendered"></h1>
+    <!-- <li v-for="(id,date,title,content) in user">{{user.id}}{{user.date}}{{user.title}}{{user.content}}</li> -->
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import axios from 'axios'
 
 export default {
-  components: {
-    AppLogo
+  async asyncData ({ params }) {
+    const { data: user } = await axios.get(`http://zd3e02.sim.zdrv.com/wp02/wp-json/wp/v2/posts/5`)
+    // const { data: user } = await axios.get(`https://api.github.com/users/${params.id}`)
+    return {
+      user
+    }
+    console.log(data);
   }
 }
 </script>
@@ -39,27 +28,12 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction: column;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.container img{
+  margin-bottom: 20px;
+  border-radius: 50%;
+  overflow: hidden;
 }
 </style>
-
